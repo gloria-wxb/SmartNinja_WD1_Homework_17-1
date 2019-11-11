dd9dc2acbd127933325210bc3a5f4550caa36af0
Gloria WXB <gloria.weixelbaum@hotmail.com>
2019-11-11 15:51:22 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, make_response
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    secret_number = request.cookies.get("secret_number")
+    response = make_response(render_template("index.html"))
+    if not secret_number:
+        new_secret = random.randint(1, 30)
+        response.set_cookie("secret_number", str(new_secret))
+
+    return response
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    try:
+        guess = int(request.form.get("my_guess"))
+    except ValueError as err:
+        print("No Value.", err)
+        return render_template("index.html")
+
+    secret_number = int(request.cookies.get("secret_number"))
+
+    if guess == secret_number:
+        correct_hl = "Congratulations!!!"
+        correct_message = "Correct! The secret number is {0}".format(str(secret_number))
+        response = make_response(render_template("result.html", correct=correct_message, correct_hl=correct_hl))
+        response.set_cookie("secret_number", str(random.randint(1, 30)))
+        return response
+
+    elif guess > secret_number:
+        message = "Try something smaller..."
+        return render_template("result.html", message=message)
+    elif guess < secret_number:
+        message = "Try something bigger..."
+        return render_template("result.html", message=message)
+    elif not guess:
+        return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
